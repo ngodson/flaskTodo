25e6a4ed1dcc958ef5b7800d979aa7ec14d63fce
ngodson <ngodson@outlook.com>
2024-03-04 12:06:39 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+port = 5001
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        task_content = request.form["content"]
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+
+        except SyntaxError:
+            return "There was an issue adding this task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:id1>")
+def delete(id1):
+    task_to_delete = Todo.query.get_or_404(id1)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except SyntaxError:
+        return "Error with this action"
+
+
+@app.route("/update/<int:id>", methods=["POST", "GET"])
+def update(id):
+    tasks = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        tasks.content = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect("/")
+        except SyntaxError:
+            return "Error updating this content"
+    else:
+        return render_template("update.html", task=tasks)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=port)
